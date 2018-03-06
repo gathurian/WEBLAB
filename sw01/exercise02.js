b5ee0e4e158ee3364d63c5aac3d98dd2f6636235
Alex <mainzel@windowslive.com>
2018-03-06 17:32:29 +0100

    Finished SW01/Exercise02

--- sw01/exercise02.js
+++ sw01/exercise02.js
@@ -1,32 +1,90 @@
-/*---------Closures------------*/
-var myStack = [];
 
-myStack.push = function (o) {
-  myStack.push(o);
-};
+// strict mode Version der Sprache
+"use strict";
 
-/*---------Object Hierarchies---------------*/
-var Circle = {
-    xPos: 0,
-    yPos: 0,
-    radius: 0
-};
+console.log("\nStart of Script \n ")
 
-function Shape(xPos, yPos) {
-    this.draw = function () {
-        console.log("Shape at Position (" + Shape.xPos + "," + Shape.yPos + ")");
+
+/*
+Erzeugen Sie ein Objekt myStack, dass die Methoden push() und pop() enthält,
+ um Objekte zu einem Stack hinzuzufügen und wieder zu entfernen.
+Der eigentliche Stack sollte nur über dieses Objekt zugreifbar sein.
+
+ */
+var myStack = (
+    function() {
+        var Stack = [];
+        return {
+            push: function (obj) {
+                Stack.push(obj);
+            },
+            pop: function () {
+                return Stack.pop();
+            }
+        };
+    }
+);
+
+/*
+    Erzeugen Sie eine Hierarchie von zeichenbaren Objekten Shape, Rectangle und Circle.
+    Alle Objekte haben eine Position x und y, sowie eine Methode draw, die ausgibt welches Objekte
+    mit welchen Attributen gezeichnet wird. I.e. Circle at Position (2, 3) mit Radius 5..
+    Definieren Sie Konstruktoren für Ihre Objekte und fügen sie die notwendigen Attribute und Funktionen hinzu.
+*/
+function Shape(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+function Circle(x, y, r){
+    this.circle = new Shape(x, y);
+    this.radius = r;
+    this.draw = function() {
+        console.log("Circle at Position (" + this.circle.x + ", " + this.circle.y + ") with radius " + this.radius);
     }
 }
 
-function Rectangle(xPos, yPos, xLenght, yLength) {
-    Shape.call(this)
-    this.xPos = xPos;
-    this.yPos = yPos;
+var circle = new Circle(1, 1, 2);
+circle.draw();
 
+function Rectangle(x, y, a, b){
+    this.rectangle = new Shape(x, y);
+    this.a = a;
+    this.b = b;
+    this.draw = function() {
+        console.log("Rectangle at Position (" + this.rectangle.x + ", " + this.rectangle.y +
+            ") with side a = " + this.a + " and side b = " + this.b);
+    }
 }
 
-Rectangle.draw();
+var rectangle = new Rectangle(2, 2, 3, 4);
+rectangle.draw();
 
-function Circle(xPos, yPos, radius) {
-    console.log("Circle at Position (" + Shape.xPos + "," + Shape.yPos + ") with radius " + Circle.radius);
-}vi .
+/*
+    Derived Properties. Fügen Sie ein Property area hinzu, das abgefragt werden kann.
+    Der Wert des Properties soll aus den anderen Properties des Objekts berechnet werden.
+*/
+
+/*Auf Rectangle.propotype, da die Funktion dann auf ALLEN Rectangles, die jemals erstellt werden könnten verfügbar ist*/
+Rectangle.prototype.area = function() {
+    return this.a * this.b;
+};
+
+console.log("Area is " + rectangle.area());
+
+
+/*
+    Serialisieren Sie die Objekte in JSON.
+    Stellen Sie sicher, dass das Read-Only Property area nicht serialisiert wird.
+
+    Was ist der Unterschied zwischen Objekt-Hierarchien in JavaScript und Vererbung in anderen OO Programmiersprachen? Was sind die Vor- und Nachteile?
+
+
+ */
+var myJSON = JSON.stringify(rectangle);
+console.log(myJSON);
+
+
+console.log("\nEnd of Script\n ");
+
+
